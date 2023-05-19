bea2309b7a4580fda9b9996b89e182fffeb68e04
unknown <dulminiwijekoon@gmail.com>
2023-05-19 10:23:29 +0530

    clear imports

--- src/main/java/com/uom/saas/controller/ProviderController.java
+++ src/main/java/com/uom/saas/controller/ProviderController.java
@@ -1,10 +1,8 @@
 package com.uom.saas.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
