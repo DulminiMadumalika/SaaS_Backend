9467b5f00e747d5717d1d13826a478e3a7915691
uddeepa <uddeepa@zilingo.com>
2023-05-20 18:54:18 +0530

    Modified type of the id variable

--- src/main/java/com/uom/saas/repository/ReviewRepository.java
+++ src/main/java/com/uom/saas/repository/ReviewRepository.java
@@ -1,9 +1,10 @@
 package com.uom.saas.repository;
 
 import com.uom.saas.entiry.Review;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReviewRepository extends MongoRepository<Review, Integer> {
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 }
